16b2805657297813fa7b9dc8fd0a8ce3b611f5bb
vijay paul <vjpaul03@gmail.com>
2026-05-27 16:56:02 +0530

    fixed error rename

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { SplitText } from "gsap/SplitText";
 import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const About = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl text-white">VIJAY PAUL</h1>
+      <h1 className="text-3xl text-white">gaurav pookie</h1>
     </div>
   )
 }
