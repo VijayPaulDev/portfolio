4b0ceb3a0ba95315f2fc1cc00af4662f62037564
vijay paul <vjpaul03@gmail.com>
2026-05-31 03:14:56 +0530

    added projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,9 +12,9 @@ const Work = () => {
           <span className="block mt-4 h-px w-full bg-white/30"></span>
         </div> 
 
-        <div className='mocktails grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 items-start'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 items-start'>
           
-          <div className='text-content w-full'>
+          <div className='Mocktails text-content w-full pb-60 pt-30'>
             <h2 className='text-2xl sm:text-3xl font-bold tracking-tight mb-6'>
               1- Mocktails Brochure Website :
             </h2>
@@ -31,13 +31,65 @@ const Work = () => {
             </div>
           </div>
 
-          <div className='image-container w-full flex justify-center lg:justify-end'>
+          <div className='image-container w-full flex justify-center lg:justify-end pt-30'>
             <img 
               className='mocktailIMG w-full max-w-xl h-auto rounded-lg shadow-2xl object-cover border border-white/10' 
               src='src/images/mocktail.png' 
               alt="Mocktail Project"
             />
           </div>
+
+        <div className='Weather text-content w-full pb-80'>
+            <h2 className='text-2xl sm:text-3xl font-bold tracking-tight mb-6'>
+              2- Weather Application :
+            </h2>
+            <div className='text-base md:text-lg text-neutral-400 leading-relaxed space-y-6'>
+              <p>
+                - Developed a type-safe, responsive weather application utilizing React and TypeScript to deliver real-time, global forecast metrics.
+              </p>
+              <p>
+                - Integrated the OpenWeatherAPI via Axios to asynchronously fetch, structure, and render dynamic weather data based on user input.
+              </p>
+              <p>
+                - Leveraged TypeScript interfaces to strictly define API response schemas, minimizing runtime errors and ensuring code maintainability.
+              </p>
+            </div>
+          </div>
+
+          <div className='image-container w-full flex justify-center lg:justify-end'>
+            <img 
+              className='WeatherIMG w-full max-w-xl h-auto rounded-lg shadow-2xl object-cover border border-white/10' 
+              src='src\images\weather.png' 
+              alt="weather Project"
+            />
+          </div>
+
+              <div className='Todolist text-content w-full pb-80'>
+            <h2 className='text-2xl sm:text-3xl font-bold tracking-tight mb-6'>
+              3- Todolist Application :
+            </h2>
+            <div className='text-base md:text-lg text-neutral-400 leading-relaxed space-y-6'>
+              <p>
+                - A minimalist task management app built to use DOM manipulation, state persistence, and clean user interactions.
+              </p>
+              <p>
+                - Usage of React useState, Tailwind and TypeScript Libraries were used in the making of the application.
+              </p>
+              <p>
+                - Emphasizes usage of TypeScript by static typing to the language to catch errors before runtime.
+              </p>
+            </div>
+          </div>
+
+          <div className='image-container w-full flex justify-center lg:justify-end'>
+            <img 
+              className='todolistIMG w-full max-w-xl h-auto rounded-lg shadow-2xl object-cover border border-white/10' 
+              src='src\images\todolist.png' 
+              alt="todolist Project"
+            />
+          </div>
+
+
 
         </div>
     
